03a0bea0e043444ca821f38e8b3b8e8dc15e5677
aggi.dcosta <agnelo.m.dcosta@gmail.com>
2016-11-05 17:01:57 -0400

    arraystring

--- hello-world/src/HelloWorld.java
+++ hello-world/src/HelloWorld.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
 
 public class HelloWorld {
-
+	
+	public static ArrayList<String> merge(String[] words, String[] more) { 
+	
+		ArrayList<String> sentence = new ArrayList<String>(); 
+		for (String w : words) sentence.add(w);
+		for (String w : more) sentence.add(w);
+		return sentence;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Hello world");
+	String[] words = {"HI", "hello"};
+	String[] more = {"HI again", "hello again "};
+	
+	ArrayList<String> mergeThem = merge(words, more);
+	System.out.print(mergeThem);
+	
 	}
 
 }
